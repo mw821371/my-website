1c9310c9e78385e148221e8d0c348e6588a76737
mw821371 <108600157+mw821371@users.noreply.github.com>
2022-07-03 08:10:45 +0500

    Add files via upload

---
+++ signup.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+int main() {
+    printf("hello world");
+//
+    int number = 25;
+    char star = '*'
+    int age = 22;
+age =24
+float pi = 3.14;
+int _age = 22;
+int final_price = 100;
+
+
+    return 0;
+}
